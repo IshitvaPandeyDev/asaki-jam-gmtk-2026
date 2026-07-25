5a2369b0f4bd61ddb882290d31324d8c60080a3c
Krishna Tyagi <krishna.tyagi.dev@gmail.com>
2026-07-26 04:16:07 +0530

    Working on Main Menu Screen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,7 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject optionsMenu;
+
     public void Play()
     {
         SceneManager.LoadScene("Game");
@@ -14,4 +15,13 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
+    public void OpenOptions()
+    {
+        optionsMenu.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsMenu.SetActive(false);
+    }
 }
